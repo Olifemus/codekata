5a969b0fd6c3169044ce72f3ebd9cb444186c46e
e.egorov <e.egorov@cloudserver.ru>
2021-05-04 10:29:00 +0300

    day 2

--- kata2/src/main.rs
+++ kata2/src/main.rs
@@ -1,34 +1,74 @@
 use std::convert::TryInto;
 
-fn main() {}
+fn main() {
+    binary(&2, &[1, 3, 5, 7]);
+}
+
+// fn binary(target: &isize, data: &[usize]) -> isize {
+//     if data.len() == 0 {
+//         return -1;
+//     }
+//     let middle = data[data.len() / 2];
+//     println!("{}", middle);
+//     let utarget = target.clone() as usize;
+//     if utarget > data[data.len() - 1] {
+//         return -1;
+//     }
+//     if middle == utarget {
+//         println!("{}", middle);
+//         return (data.len() / 2).try_into().unwrap();
+//     } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
+//         return (data.len() / 2 + 1).try_into().unwrap();
+//     } else {
+//         let mut min = 0;
+//         let mut max = data.len() - 1;
+//         if middle > utarget {
+//             println!("HIGHER");
+//             max = data.len() / 2;
+//         } else {
+//             println!("LOWER");
+//             min = data.len() / 2;
+//         }
+//         println!("{} {}", min, max);
+//         return binary(target, &data[min..max]);
+//     }
+// }
 
 fn binary(target: &isize, data: &[usize]) -> isize {
+    println!("--------------------------------");
+    println!("{:?}", data);
+    println!("{}", target);
     if data.len() == 0 {
         return -1;
     }
-    let middle = data[data.len() / 2];
-    println!("{}", middle);
+    let mut min = 0;
+    let mut max = data.len() - 1;
     let utarget = target.clone() as usize;
     if utarget > data[data.len() - 1] {
         return -1;
     }
-    if middle == utarget {
-        println!("{}", middle);
-        return (data.len() / 2).try_into().unwrap();
-    } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
-        return (data.len() / 2 + 1).try_into().unwrap();
-    } else {
-        let mut min = 0;
-        let mut max = data.len() - 1;
-        if middle > utarget {
-            println!("HIGHER");
-            max = data.len() / 2;
+    loop {
+        let marker = (min + max) / 2;
+        let middle = data[marker];
+        println!("{}, {}", marker, middle);
+        if middle == utarget {
+            println!("finded: {}", middle);
+            return marker.try_into().unwrap();
+        } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
+            return (data.len() / 2 + 1).try_into().unwrap();
         } else {
-            println!("LOWER");
-            min = data.len() / 2;
+            if middle > utarget {
+                println!("HIGHER");
+                max = (max - min) / 2;
+            } else {
+                println!("LOWER");
+                min = (max - min) / 2;
+            }
+            if (min + max) / 2 == marker {
+                return -1;
+            }
+            println!("bounds : {}, {}", min, max);
         }
-        println!("{} {}", min, max);
-        return binary(target, &data[min..max]);
     }
 }
 
--- kata2/src/test.rs
+++ kata2/src/test.rs
@@ -13,7 +13,6 @@ fn test_bsearch() {
     assert_eq!(-1, binary(&4, &[1, 3, 5]));
     assert_eq!(-1, binary(&6, &[1, 3, 5]));
     ///
-    assert_eq!(0, binary(&1, &[1, 3, 5, 7]));
     assert_eq!(1, binary(&3, &[1, 3, 5, 7]));
     assert_eq!(2, binary(&5, &[1, 3, 5, 7]));
     assert_eq!(3, binary(&7, &[1, 3, 5, 7]));
