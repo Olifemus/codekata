e253c19217f4c7364c4301cfe7fb28a2babdc025
e.egorov <e.egorov@cloudserver.ru>
2021-05-06 00:06:44 +0300

    day 3

--- kata2/src/main.rs
+++ kata2/src/main.rs
@@ -1,7 +1,7 @@
 use std::convert::TryInto;
 
 fn main() {
-    binary(&2, &[1, 3, 5, 7]);
+    println!("result {}", binary(&6, &[1, 3, 5, 7]));
 }
 
 // fn binary(target: &isize, data: &[usize]) -> isize {
@@ -34,6 +34,44 @@ fn main() {
 //     }
 // }
 
+// fn binary(target: &isize, data: &[usize]) -> isize {
+//     println!("--------------------------------");
+//     println!("{:?}", data);
+//     println!("{}", target);
+// if data.len() == 0 {
+//     return -1;
+// }
+//     let mut min = 0;
+//     let mut max = data.len() - 1;
+// let utarget = target.clone() as usize;
+//     if utarget > data[data.len() - 1] {
+//         return -1;
+//     }
+//     loop {
+//         let marker = (min + max) / 2;
+//         let middle = data[marker];
+//         println!("{}, {}", marker, middle);
+//         if middle == utarget {
+//             println!("finded: {}", middle);
+//             return marker.try_into().unwrap();
+//         } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
+//             return (data.len() / 2 + 1).try_into().unwrap();
+//         } else {
+//             if middle > utarget {
+//                 println!("HIGHER");
+//                 max = (max - min) / 2;
+//             } else {
+//                 println!("LOWER");
+//                 min = (max - min) / 2;
+//             }
+//             if (min + max) / 2 == marker {
+//                 return -1;
+//             }
+//             println!("bounds : {}, {}", min, max);
+//         }
+//     }
+// }
+
 fn binary(target: &isize, data: &[usize]) -> isize {
     println!("--------------------------------");
     println!("{:?}", data);
@@ -41,33 +79,32 @@ fn binary(target: &isize, data: &[usize]) -> isize {
     if data.len() == 0 {
         return -1;
     }
-    let mut min = 0;
-    let mut max = data.len() - 1;
     let utarget = target.clone() as usize;
-    if utarget > data[data.len() - 1] {
-        return -1;
-    }
+    let mut prev_marker;
+    let mut marker = data.len() / 2;
+    let mut step = if marker / 2 < 1 { 1 } else { marker / 2 };
     loop {
-        let marker = (min + max) / 2;
-        let middle = data[marker];
-        println!("{}, {}", marker, middle);
-        if middle == utarget {
-            println!("finded: {}", middle);
+        if utarget == data[marker] {
             return marker.try_into().unwrap();
-        } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
-            return (data.len() / 2 + 1).try_into().unwrap();
+        }
+        prev_marker = marker;
+        if utarget > data[marker] {
+            println!("lower");
+            marker = marker + step;
         } else {
-            if middle > utarget {
-                println!("HIGHER");
-                max = (max - min) / 2;
-            } else {
-                println!("LOWER");
-                min = (max - min) / 2;
-            }
-            if (min + max) / 2 == marker {
+            println!("highter");
+            if step > marker {
                 return -1;
             }
-            println!("bounds : {}, {}", min, max);
+            marker = marker - step;
+        }
+        step = if step / 2 < 1 { 1 } else { step / 2 };
+        println!("nextMarker: {}, prevMarker: {}", marker, prev_marker);
+        if marker >= data.len() || prev_marker == marker {
+            return -1;
+        }
+        if utarget < data[marker] && utarget > data[prev_marker] {
+            return -1;
         }
     }
 }
--- kata2/src/test.rs
+++ kata2/src/test.rs
@@ -1,10 +1,10 @@
 use super::*;
 #[test]
-fn test_bsearch() {
+fn test_bsearch_1() {
     assert_eq!(-1, binary(&3, &[]));
     assert_eq!(-1, binary(&3, &[1]));
     assert_eq!(0, binary(&1, &[1]));
-    ///
+    //
     assert_eq!(0, binary(&1, &[1, 3, 5]));
     assert_eq!(1, binary(&3, &[1, 3, 5]));
     assert_eq!(2, binary(&5, &[1, 3, 5]));
@@ -12,13 +12,17 @@ fn test_bsearch() {
     assert_eq!(-1, binary(&2, &[1, 3, 5]));
     assert_eq!(-1, binary(&4, &[1, 3, 5]));
     assert_eq!(-1, binary(&6, &[1, 3, 5]));
-    ///
+    //
     assert_eq!(1, binary(&3, &[1, 3, 5, 7]));
     assert_eq!(2, binary(&5, &[1, 3, 5, 7]));
     assert_eq!(3, binary(&7, &[1, 3, 5, 7]));
     assert_eq!(-1, binary(&0, &[1, 3, 5, 7]));
     assert_eq!(-1, binary(&2, &[1, 3, 5, 7]));
     assert_eq!(-1, binary(&4, &[1, 3, 5, 7]));
-    assert_eq!(-1, binary(&6, &[1, 3, 5, 7]));
     assert_eq!(-1, binary(&8, &[1, 3, 5, 7]));
+}
+
+#[test]
+fn test_bsearch_2() {
+    assert_eq!(-1, binary(&6, &[1, 3, 5, 7]));
 }
