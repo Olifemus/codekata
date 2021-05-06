811cc95558350682ed7cdb623cccd50b38b15cd2
e.egorov <e.egorov@cloudserver.ru>
2021-05-06 11:28:19 +0300

    fix for day 1 interpretaton

--- kata2/src/main.rs
+++ kata2/src/main.rs
@@ -1,7 +1,8 @@
 use std::convert::TryInto;
 
 fn main() {
-    println!("result {}", binary(&6, &[1, 3, 5, 7]));
+    let longvec = vec![1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+    println!("result {}", longvec[binary(&17, &longvec) as usize]);
 }
 
 // fn binary(target: &isize, data: &[usize]) -> isize {
@@ -20,6 +21,7 @@ fn main() {
 //     } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
 //         return (data.len() / 2 + 1).try_into().unwrap();
 //     } else {
+//         let mut border: isize = 0;
 //         let mut min = 0;
 //         let mut max = data.len() - 1;
 //         if middle > utarget {
@@ -28,47 +30,10 @@ fn main() {
 //         } else {
 //             println!("LOWER");
 //             min = data.len() / 2;
+//             border = min.clone().try_into().unwrap();
 //         }
 //         println!("{} {}", min, max);
-//         return binary(target, &data[min..max]);
-//     }
-// }
-
-// fn binary(target: &isize, data: &[usize]) -> isize {
-//     println!("--------------------------------");
-//     println!("{:?}", data);
-//     println!("{}", target);
-// if data.len() == 0 {
-//     return -1;
-// }
-//     let mut min = 0;
-//     let mut max = data.len() - 1;
-// let utarget = target.clone() as usize;
-//     if utarget > data[data.len() - 1] {
-//         return -1;
-//     }
-//     loop {
-//         let marker = (min + max) / 2;
-//         let middle = data[marker];
-//         println!("{}, {}", marker, middle);
-//         if middle == utarget {
-//             println!("finded: {}", middle);
-//             return marker.try_into().unwrap();
-//         } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
-//             return (data.len() / 2 + 1).try_into().unwrap();
-//         } else {
-//             if middle > utarget {
-//                 println!("HIGHER");
-//                 max = (max - min) / 2;
-//             } else {
-//                 println!("LOWER");
-//                 min = (max - min) / 2;
-//             }
-//             if (min + max) / 2 == marker {
-//                 return -1;
-//             }
-//             println!("bounds : {}, {}", min, max);
-//         }
+//         return  border + binary(target, &data[min..max]);
 //     }
 // }
 
@@ -79,33 +44,103 @@ fn binary(target: &isize, data: &[usize]) -> isize {
     if data.len() == 0 {
         return -1;
     }
+    let mut min = 0;
+    let mut max = data.len() - 1;
     let utarget = target.clone() as usize;
-    let mut prev_marker;
-    let mut marker = data.len() / 2;
-    let mut step = if marker / 2 < 1 { 1 } else { marker / 2 };
+    if utarget > data[data.len() - 1] {
+        return -1;
+    }
     loop {
-        if utarget == data[marker] {
+        let marker = (min + max) / 2;
+        let middle = data[marker];
+        println!("{}, {}", marker, middle);
+        if middle == utarget {
+            println!("finded: {}", middle);
             return marker.try_into().unwrap();
-        }
-        prev_marker = marker;
-        if utarget > data[marker] {
-            println!("lower");
-            marker = marker + step;
+        } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
+            return (data.len() / 2 + 1).try_into().unwrap();
         } else {
-            println!("highter");
-            if step > marker {
+            if middle > utarget {
+                println!("HIGHER");
+                max = (max - min) / 2;
+            } else {
+                println!("LOWER");
+                min = (max - min) / 2;
+            }
+            if (min + max) / 2 == marker {
                 return -1;
             }
-            marker = marker - step;
+            println!("bounds : {}, {}", min, max);
         }
-        step = if step / 2 < 1 { 1 } else { step / 2 };
-        println!("nextMarker: {}, prevMarker: {}", marker, prev_marker);
-        if marker >= data.len() || prev_marker == marker {
-            return -1;
+    }
+}
+
+// fn binary(target: &isize, data: &[usize]) -> isize {
+//     println!("--------------------------------");
+//     println!("{:?}", data);
+//     println!("{}", target);
+//     if data.len() == 0 {
+//         return -1;
+//     }
+//     let utarget = target.clone() as usize;
+//     let mut prev_marker;
+//     let mut marker = data.len() / 2;
+//     let mut step = if marker / 2 < 1 { 1 } else { marker / 2 };
+//     loop {
+//         if utarget == data[marker] {
+//             return marker.try_into().unwrap();
+//         }
+//         prev_marker = marker;
+//         if utarget > data[marker] {
+//             println!("lower");
+//             marker = marker + step;
+//         } else {
+//             println!("highter");
+//             if step > marker {
+//                 return -1;
+//             }
+//             marker = marker - step;
+//         }
+//         step = if step / 2 < 1 { 1 } else { step / 2 };
+//         println!("nextMarker: {}, prevMarker: {}", marker, prev_marker);
+//         if marker >= data.len() || prev_marker == marker {
+//             return -1;
+//         }
+//         if utarget < data[marker] && utarget > data[prev_marker] {
+//             return -1;
+//         }
+//     }
+// }
+
+fn binary(target: &isize, data: &[usize]) -> isize {
+    if data.len() == 0 {
+        return -1;
+    }
+    let middle = data[data.len() / 2];
+    println!("{}", middle);
+    let utarget = target.clone() as usize;
+    if utarget > data[data.len() - 1] {
+        return -1;
+    }
+    if middle == utarget {
+        println!("{}", middle);
+        return (data.len() / 2).try_into().unwrap();
+    } else if middle != data[data.len() - 1] && data[data.len() / 2 + 1] == utarget {
+        return (data.len() / 2 + 1).try_into().unwrap();
+    } else {
+        let mut border: isize = 0;
+        let mut min = 0;
+        let mut max = data.len() - 1;
+        if middle > utarget {
+            println!("HIGHER");
+            max = data.len() / 2;
+        } else {
+            println!("LOWER");
+            min = data.len() / 2;
+            border = min.clone().try_into().unwrap();
         }
-        if utarget < data[marker] && utarget > data[prev_marker] {
-            return -1;
-        }
+        println!("{} {}", min, max);
+        return border + binary(target, &data[min..max]);
     }
 }
 
